75a54be1f184482c5168919b3d724783cdf7b9a1
tzviky <tzviky@gmail.com>
2026-06-07 22:37:25 +0300

    first upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='diro2000-bot-final-v1';const FILES=['./','./index.html','./manifest.json','./icon.svg','./bot.svg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
